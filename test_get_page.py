15c0e19845970d58746084597fbb69b79d409a95
Oleh Herasym <oolleehh@gmail.com>
2019-08-16 08:44:42 +0300

    444

--- test_get_page.py
+++ test_get_page.py
@@ -2,16 +2,20 @@ import urllib.request
 import urllib.parse
 import time
 import json
+from datetime import datetime
 
-#TODO: add photos parsing
-#TODO: add total edges count
-#TODO: create top 10 photo likes
-#TODO: create top 10 video likes
-
+#TODO: if photo/video has less than 7 days, show warning about likes
+#TODO: calculate in which time account post photos or videos
+#TODO: calculate days of the week when account post photos
+#TODO: in which time people comment edges????
+#TODO: TOP 10 likers
+#TODO: TOP 10 commenters
 
 class PageAnalytics:
     def __init__(self):
         self.username_id = ''
+        self.likes = []
+        self.photo_likes = []
 
 
     def get_page(self, url):
@@ -28,6 +32,34 @@ class PageAnalytics:
         json_page = json.loads(page)
         return json_page['graphql']['user']['id']
 
+    def get_total_edges(self, username):
+        page = self.get_page('https://www.instagram.com/' + username + '/?__a=1')
+        json_page = json.loads(page)
+        return json_page['graphql']['user']['edge_owner_to_timeline_media']['count']
+
+    def is_page_private(self, username):
+        page = self.get_page('https://www.instagram.com/' + username + '/?__a=1')
+        json_page = json.loads(page)
+        if json_page['graphql']['user']['is_private']:
+            return True
+        return False
+
+    def total_likes(self):
+        count = 0
+        for edge in self.photo_likes:
+            count += edge['likes']
+        for edge in self.likes:
+            count += edge['likes']
+        return count
+
+    def total_comments(self):
+        count = 0
+        for edge in self.photo_likes:
+            count += edge['comments_count']
+        for edge in self.likes:
+            count += edge['comments_count']
+        return count
+
 
     def get_cursor(self, page):
         find_str = '"page_info":{"has_next_page":true,"end_cursor":"'
@@ -38,8 +70,6 @@ class PageAnalytics:
 
         cursor = page[start_index + len(find_str): start_index + len(find_str) + end_index]
         return cursor
-
-
 
 
     def prepare_params(self, cursor):
@@ -54,25 +84,100 @@ class PageAnalytics:
 
         query_string = urllib.parse.urlencode(params)
         query_string = query_string.replace('+', '').replace('27', '22')
-
         return query_string
 
+    def _get_graph_image_object(self, edge):
+        text = ''
+        if len(edge['node']['edge_media_to_caption']['edges']):
+            text = edge['node']['edge_media_to_caption']['edges'][0]['node']['text']
 
+        timestamp = edge['node']['taken_at_timestamp']
+        dt_object = time.ctime(timestamp)
+
+        photo_data = {
+            'likes' : edge['node']['edge_media_preview_like']['count'],
+            'url' : edge['node']['display_url'],
+            'comments_count' : edge['node']['edge_media_to_comment']['count'],
+            'timestamp' : dt_object,
+            'text' : text
+        }
+        return photo_data
+
+    def _get_graph_video_object(self, edge):
+        text = ''
+        if len(edge['node']['edge_media_to_caption']['edges']):
+            text = edge['node']['edge_media_to_caption']['edges'][0]['node']['text']
+
+        timestamp = edge['node']['taken_at_timestamp']
+        dt_object = time.ctime(timestamp)
+
+        data = {
+            "likes" : edge['node']['edge_media_preview_like']['count'],
+            # "url" : edge['node']['video_url'],
+            "url" : edge['node']['display_url'],
+            "video_view_count" : edge['node']['video_view_count'],
+            "comments_count" : edge['node']['edge_media_to_comment']['count'],
+            "timestamp" : dt_object,
+            "text" : text
+        }
+        return data
+
+    def _parse_edges(self, edges):
+        for edge in edges:
+            # try:
+                if edge['node']['__typename'] == 'GraphImage':
+                    photo_data = self._get_graph_image_object(edge)
+                    
+                    print(photo_data)
+                    self.photo_likes.append(photo_data)
+
+                elif edge['node']['__typename'] == 'GraphVideo':
+                    data = self._get_graph_video_object(edge)
+
+                    print(data)
+                    self.likes.append(data)
+                else:
+                    data = self._get_graph_image_object(edge)
+                    print(data)
+                    self.photo_likes.append(data)
+            # except:
+                # print('Some parse error')
+                # print(edge)
+                # time.sleep(10000)
+
+    def _get_first_page_json(self, page):
+        find_str = 'window._sharedData = {"config":'
+
+        start_index = page.find(find_str)
+        end_index = page[start_index:].find('</script>')
+
+        page_json_only = page[start_index + len('window._sharedData = '): start_index + end_index - 1]
+        first_page_json = json.loads(page_json_only)
+
+        return first_page_json['entry_data']['ProfilePage'][0]['graphql']
 
 
     def analyze(self, username):
         self.username_id = self.get_id_by_username(username)
+        total_edges = self.get_total_edges(username)
+        print('Total edges = ', total_edges)
+
+        if self.is_page_private(username):
+            print('Page ' + username + ' is private!')
+            return
 
         page = self.get_page('https://www.instagram.com/' + username + '/')
 
+        first_page_json = self._get_first_page_json(page)
+        
+        self._parse_edges(first_page_json['user']['edge_owner_to_timeline_media']['edges'])
+
         cursor = self.get_cursor(page)
-        #get total edges count
-        #get text from json
+        print('cursor = ', cursor)
 
-        likes = []
-        photo_likes = []
+        requests_number = int(total_edges - 12 / 50) + 1
 
-        for i in range(0, 10):
+        for i in range(0, requests_number):
             time.sleep(3)
             
             try:
@@ -82,49 +187,38 @@ class PageAnalytics:
                 print('Request error')
                 break
 
-            for edge in json_page['data']['user']['edge_owner_to_timeline_media']['edges']:
-                try:
-                    if edge['node']['__typename'] == 'GraphImage':
-                        photo_data = {
-                            'likes' : edge['node']['edge_media_preview_like']['count'],
-                        }
-                        print(photo_data)
-                        photo_likes.append(photo_data)
-
-                    elif edge['node']['__typename'] == 'GraphVideo':
-                        data = {
-                            "likes" : edge['node']['edge_media_preview_like']['count'],
-                            "url" : edge['node']['video_url'],
-                            "video_view_count" : edge['node']['video_view_count'],
-                            "comments_count" : edge['node']['edge_media_to_comment']['count'],
-                            "timestamp" : edge['node']['taken_at_timestamp'],
-                            # "text" : edge['node']['edge_media_to_caption']['edges']['node']['text']
-                        }
-
-                        print(data)
-                        likes.append(data)
-                    else:
-                        print('__typename = ', edge['node']['__typename'])
-                except:
-                    print('Some parse error')
-                    print(edge)
-                    time.sleep(10000)
+            self._parse_edges(json_page['data']['user']['edge_owner_to_timeline_media']['edges'])
 
             cursor = json_page['data']['user']['edge_owner_to_timeline_media']['page_info']['end_cursor']
 
 
-        likes.sort(key=lambda x:x['likes'])
-        photo_likes.sort(key=lambda x:x['likes'])
+
+        self.likes.sort(key=lambda x:x['likes'])
+        self.photo_likes.sort(key=lambda x:x['likes'])
 
 
-        print('PHOTO LIKES')
+        print('\n\nTOP 10 LOW LIKES PHOTOS')
+        for like in self.photo_likes[:10]:
+            print(like)
 
-        for like in photo_likes:
+        print('\n\nTOP 10 LOW LIKES VIDEOS')
+        for like in self.likes[:10]:
+            print(like)
+
+        print('\n\nPHOTOS TOP 10 LIKES')
+
+        for like in self.photo_likes[-10:]:
             print(like)
 
         print('\n\n\nVIDEO LIKES')
-        for like in likes:
+        for like in self.likes[-10:]:
             print(like)
+
+        print('photo count = ', len(self.photo_likes))
+        print('video count = ', len(self.likes))
+        print('Total likes = ', self.total_likes())
+        print('Total comments = ', self.total_comments())
+        print('\n\nTotal = ', len(self.photo_likes) + len(self.likes))
 
 
 
