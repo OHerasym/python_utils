06a34c9ffe7d2f099051d4d04038955595e20a1c
Oleh Herasym <oolleehh@gmail.com>
2022-08-14 14:29:54 +0300

    fix spaces

--- check_import.py
+++ check_import.py
@@ -4,16 +4,16 @@ import sys
 
 
 def autoimport(package_name):
-	try:
-		__import__(package_name)
-	except ModuleNotFoundError as e:
-		logger.error(e)
-		logger.info('Trying to install ' + package_name + ' package')
+    try:
+        __import__(package_name)
+    except ModuleNotFoundError as e:
+        logger.error(e)
+        logger.info('Trying to install ' + package_name + ' package')
 
-		subprocess.check_call([sys.executable, '-m', 'pip', 'install', package_name])
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', package_name])
 
-		__import__(package_name)
-		logger.info('Package '+ package_name + ' is installed')
+        __import__(package_name)
+        logger.info('Package '+ package_name + ' is installed')
 
 
 
--- timer.py
+++ timer.py
@@ -4,42 +4,42 @@ from threading import Thread
 import time
 
 class Timer:
-	def __init__(self, sleep_time, func, single_shot=False):
-		self.thread = Thread(target=self._internal_thread)
-		self._event = threading.Event()
-		self._func = func
-		self._result_func = None
-		self.sleep_time = sleep_time
-		self.single_shot = single_shot
+    def __init__(self, sleep_time, func, single_shot=False):
+        self.thread = Thread(target=self._internal_thread)
+        self._event = threading.Event()
+        self._func = func
+        self._result_func = None
+        self.sleep_time = sleep_time
+        self.single_shot = single_shot
 
-	def set_result_func(self, func):
-		self._result_func = func
+    def set_result_func(self, func):
+        self._result_func = func
 
-	def _internal_thread(self):
-		while not self._event.wait(self.sleep_time):
-			result = self._func()
+    def _internal_thread(self):
+        while not self._event.wait(self.sleep_time):
+            result = self._func()
 
-			if result:
-				if self._result_func:
-					logger.debug('on_result call')
-					self._result_func(result)
+            if result:
+                if self._result_func:
+                    logger.debug('on_result call')
+                    self._result_func(result)
 
-			if self.single_shot:
-				break
+            if self.single_shot:
+                break
 
-	def start(self):
-		self.thread.start()
+    def start(self):
+        self.thread.start()
 
-	def on_exit(self):
-		self._event.set()
+    def on_exit(self):
+        self._event.set()
 
 
 # def test_func():
-# 	print('test_func')
-# 	return 42
+#   print('test_func')
+#   return 42
 
 # def get_result(result):
-# 	print('result: ', result)
+#   print('result: ', result)
 
 # obj = Timer(2, test_func)
 # obj.set_result_func(get_result)
